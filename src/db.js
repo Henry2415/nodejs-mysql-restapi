fa6ba28a9f05ad20330a8c6a8a7cbb0a4d7b1e72
root <root@localhost.localdomain>
2023-10-21 14:10:49 +0000

    add port mariadb

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config";
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config.js";
 
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
+    port: DB_PORT,
     password: DB_PASSWORD,
     database: DB_DATABASE
 })
